49dc1583cd68698f77eabc641b64a3114a891ea5
Lubin Tan <tanlubin1986@gmail.com>
2019-11-05 10:44:52 +0800

    Removed unnecessary `console.log`s.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -16,19 +16,11 @@ module.exports = function(deployer,network, accounts) {
       { from: regulatorOwner, gas: 15000000 });
   })
   .then((txObj) => {
-    console.log( txObj.receipt.logs[1].event);
     const newOperatorContractAddr = txObj.receipt.logs[1].args.newOperator;
     return TollBoothOperator.at(newOperatorContractAddr);
   })
   .then((tbOperatorContract) => {
-    console.log(tbOperatorContract.address);
     return tbOperatorContract.setPaused(0, { from: tollBoothOpOwner, gas: 300000 });
   })
-  // .then(() => {
-  //   return depl.getPastEvents("LogTollBoothOperatorCreated", {fromBlock:0, toBlock:'latest'});
-  // })
-  // .then((res) => {
-  //   console.log(res);
-  // })
 };
 
